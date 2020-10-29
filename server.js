6459c73c0fcf4c33d0ea71b18c746d2ec79af40a
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-28 21:53:11 -0400

    add middleware and conditionals for search by get request

--- server.js
+++ server.js
@@ -3,3 +3,33 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const MOVIESTORE = require('/.movies-data-small.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
+
+app.use(function validateBearerToken(req, res, next){
+    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized Request'})
+    }
+
+    next()
+
+})
+
+app.get('/movie', (req, res)=>{
+   if(req.query.genre){
+    
+   }
+   if(req.query.country){
+    
+   }
+   if(req.query.avg_note){
+    
+   } 
+})
